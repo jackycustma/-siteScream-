2b86eed416135875db9c19effb431cf30c16634f
jacqueline Custodio machado <jacquelinecustodiomachado@gmail.com>
2024-06-14 12:51:04 -0300

    Criando cards interativos

---
+++ paginas/home.php
@@ -0,0 +1,46 @@
+<div class="container1">
+            <div class="flip">
+                <div class="frente"></div>
+                <div class="tras">
+                    <p>O game é focado na Aventura e Exploração</p>
+                </div>
+            </div>
+        </div>
+        <div class="container1">
+            <div class="flip">
+                <div class="frente"></div>
+                <div class="tras">
+                    <p>O game é focado na Aventura e Exploração</p>
+                </div>
+            </div>
+        </div><div class="container1">
+            <div class="flip">
+                <div class="frente"></div>
+                <div class="tras">
+                    <p>O game é focado na Aventura e Exploração</p>
+                </div>
+            </div>
+        </div><div class="container1">
+            <div class="flip">
+                <div class="frente"></div>
+                <div class="tras">
+                    <p>O game é focado na Aventura e Exploração</p>
+                </div>
+            </div>
+        </div><div class="container1">
+            <div class="flip">
+                <div class="frente"></div>
+                <div class="tras">
+                    <p>O game é focado na Aventura e Exploração</p>
+                </div>
+            </div>
+        </div>
+        </div><div class="container1">
+            <div class="flip">
+                <div class="frente"></div>
+                <div class="tras">
+                    <p>O game é focado na Aventura e Exploração</p>
+                </div>
+            </div>
+        </div>
+        
