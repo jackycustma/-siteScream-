80a115ca2e97d24246d160a6d1885e8908fd7eab
jacqueline Custodio machado <jacquelinecustodiomachado@gmail.com>
2024-06-18 21:34:46 -0300

    Apagando algumas informaçoes que nao precisa

--- paginas/home.php
+++ paginas/home.php
@@ -2,6 +2,7 @@
             <div class="flip">
                 <div class="frente"></div>
                 <div class="tras">
+                    <img src="imagens/HelpSauro3d.png" class="img">
                     <p>O game é focado na Aventura e Exploração</p>
                 </div>
             </div>
@@ -10,37 +11,44 @@
             <div class="flip">
                 <div class="frente"></div>
                 <div class="tras">
-                    <p>O game é focado na Aventura e Exploração</p>
-                </div>
-            </div>
-        </div><div class="container1">
-            <div class="flip">
-                <div class="frente"></div>
-                <div class="tras">
-                    <p>O game é focado na Aventura e Exploração</p>
-                </div>
-            </div>
-        </div><div class="container1">
-            <div class="flip">
-                <div class="frente"></div>
-                <div class="tras">
-                    <p>O game é focado na Aventura e Exploração</p>
-                </div>
-            </div>
-        </div><div class="container1">
-            <div class="flip">
-                <div class="frente"></div>
-                <div class="tras">
+                    <img src="imagens/HelpSauro3d.png" class="img">
                     <p>O game é focado na Aventura e Exploração</p>
                 </div>
             </div>
         </div>
-        </div><div class="container1">
+        <div class="container1">
             <div class="flip">
                 <div class="frente"></div>
                 <div class="tras">
+                    <img src="imagens/HelpSauro3d.png" class="img">
                     <p>O game é focado na Aventura e Exploração</p>
                 </div>
             </div>
         </div>
-        
+        <div class="container1">
+            <div class="flip">
+                <div class="frente"></div>
+                <div class="tras">
+                    <img src="imagens/HelpSauro3d.png" class="img">
+                    <p>O game é focado na Aventura e Exploração</p>
+                </div>
+            </div>
+        </div>
+        <div class="container1">
+            <div class="flip">
+                <div class="frente"></div>
+                <div class="tras">
+                    <img src="imagens/HelpSauro3d.png" class="img">
+                    <p>O game é focado na Aventura e Exploração</p>
+                </div>
+            </div>
+        </div>
+        <div class="container1">
+            <div class="flip">
+                <div class="frente"></div>
+                <div class="tras">
+                    <img src="imagens/HelpSauro3d.png" class="img">
+                    <p>O game é focado na Aventura e Exploração</p>
+                </div>
+            </div>
+        </div>
--- paginas/registro.php
+++ paginas/registro.php
@@ -1,17 +1,16 @@
 <?php
-// Verifica se o formulário foi enviado
+
 if($_SERVER["REQUEST_METHOD"] == "POST") {
-    // Obtém os dados do formulário
+
     $nome = $_POST['nome'];
     $email = $_POST['email'];
     $senha = $_POST['senha'];
 
-    // Aqui você pode processar os dados, como salvar no banco de dados, enviar por e-mail, etc.
-
-    // Exemplo básico: exibe os dados na tela
+  
+    
     echo "<h2>Informações Recebidas:</h2>";
     echo "<p><strong>Nome:</strong> $nome</p>";
     echo "<p><strong>E-mail:</strong> $email</p>";
-    // Não exibe a senha por questões de segurança
+    
 }
 ?>
